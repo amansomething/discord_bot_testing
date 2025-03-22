39d06b9b984676cde130e832eece69f364594d32
aman <amansomething@gmail.com>
2025-03-22 08:34:35 -0400

    Simplifying code. Adding comments

--- config.py
+++ config.py
@@ -7,6 +7,18 @@ env.read_env()
 class Config:
     token: str = env.str('DISCORD_TOKEN')
     channel_id: int = env.int('CHANNEL_ID')
+    channel = None  # Where to send messages. Needs to be initialized after bot is logged in.
+
+    def set_channel(self, channel_id=None):
+        """
+        Sets the channel where messages will be sent.
+        Uses the channel_id set in the Config unless specified otherwise.
+
+        :param channel_id: int - The channel ID to send messages to
+        """
+        if channel_id is None:
+            channel_id = self.channel_id
+        self.channel = client.get_channel(channel_id)
 
 
 config = Config()
--- main.py
+++ main.py
@@ -2,52 +2,33 @@ import discord
 from config import config, client
 
 
-async def send_event_message(message):
-    channel = client.get_channel(config.channel_id)
-    print(f"Sending message: {message}")
-    await channel.send(message)
-
 @client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
+async def on_ready() -> None:
+    """Log in as the user and set the channel to send messages to."""
+    print(f'Logged in as: {client.user}')
+    config.set_channel()
+    print(f'Messages will be sent to: {config.channel}')
 
-# Check if new event was added
+
 @client.event
 async def on_audit_log_entry_create(entry):
+    """
+    Check if a new event was added, updated, or deleted.
+    Send a message to the channel if any of these actions occur.
+
+    :param entry: discord.AuditLogEntry - The entry that was created
+    """
+    channel = config.channel
     event = entry.target
 
     if entry.action == discord.AuditLogAction.scheduled_event_create:
-        await send_event_message(f"**New event created!**")
-        await event_send_details(event)
+        await channel.send(f"**New event created!**\n{event.url}")
     elif entry.action == discord.AuditLogAction.scheduled_event_update:
-        await send_event_message(f"**An event was updated!**")
-        await event_send_details(event)
+        await channel.send(f"**An event was updated!**\n{event.url}")
     elif entry.action == discord.AuditLogAction.scheduled_event_delete:
-        await send_event_message(f"Heads up! `{entry.changes.before.name}` has been cancelled!")
+        await channel.send(f"Heads up! `{entry.changes.before.name}` has been cancelled!")
     else:
         print("Something else happened that we don't care about.")
-
-
-async def event_send_details(event):
-    """
-    https://discordpy.readthedocs.io/en/stable/api.html?highlight=event#discord.ScheduledEvent
-    """
-    # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
-    time_format = "%I:%M%p"
-    day = event.start_time.strftime("%A, %b %m, %Y")
-    start = event.start_time.strftime(time_format)
-    end = event.end_time.strftime(time_format)
-    message = f"""
-**Event**: [{event.name}]({event.url})
-**Location**: {event.location}
-
-**Description**: {event.description}
-
-**Time**: {day} @ {start} - {end}
-```
-
-"""
-    await send_event_message(message)
 
 
 client.run(config.token)
