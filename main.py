4dadbf14871a01bae4220751a6922d7d904d4e0e
aman <amansomething@gmail.com>
2025-08-04 20:40:37 -0400

    Removed update notifications and timed checks.

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def on_audit_log_entry_create(
 
     relevant_actions = {
         discord.AuditLogAction.scheduled_event_create: "New",
-        discord.AuditLogAction.scheduled_event_update: "Updated",
+        # discord.AuditLogAction.scheduled_event_update: "Updated",
         discord.AuditLogAction.scheduled_event_delete: "Cancelled",
     }
 
@@ -90,42 +90,42 @@ async def on_audit_log_entry_create(
         )
 
 
-@tasks.loop(minutes=config.event_check_interval)
-async def check_events(guild: discord.Guild = None):
-    """
-    Checks for scheduled events ending within a threshold and creates a thread if so.
-
-    :param guild: The Discord guild (server) to check for scheduled events.
-    """
-    guild = guild or config.guild  # Use the guild from config if not provided
-
-    now = datetime.now(timezone.utc)
-    print(f"Checking for scheduled events... {now.isoformat()}")
-    # Calculate the threshold time for creating a thread
-    threshold_time = now + timedelta(minutes=config.event_completion_threshold)
-
-    for event in guild.scheduled_events:
-        # Check if the event has an end time and if it falls within the threshold
-        ending_soon = event.end_time and now <= event.end_time <= threshold_time
-
-        if not ending_soon:
-            print(f"Event '{event.name}' is not ending soon. End time: {event.end_time}")
-            continue
-
-        print(f"Event '{event.name}' is ending soon. Creating a discussion thread...")
-        channel = config.events_channel
-        month_year = event.end_time.strftime("%m/%d")  # Ex. "12/31" for December 31st
-        thread_name = f"{event.name} - ({month_year})"  # Ex. "Snek Den - (12/31)"
-        thread = await channel.create_thread(
-            name=thread_name,
-            type=discord.ChannelType.public_thread,
-            auto_archive_duration=config.auto_archive_duration
-        )
-        await thread.send(f"`{event.name}` is ending soon! Discuss here!")
-        print(f"Created discussion thread '{thread_name}' for event '{event.name}' with ID {event.id}.")
-
-    print(f"Finished checking for scheduled events. Will check again in: {config.event_check_interval} minutes\n")
-
-
-check_events.before_loop(bot.wait_until_ready)
+# @tasks.loop(minutes=config.event_check_interval)
+# async def check_events(guild: discord.Guild = None):
+#     """
+#     Checks for scheduled events ending within a threshold and creates a thread if so.
+#
+#     :param guild: The Discord guild (server) to check for scheduled events.
+#     """
+#     guild = guild or config.guild  # Use the guild from config if not provided
+#
+#     now = datetime.now(timezone.utc)
+#     print(f"Checking for scheduled events... {now.isoformat()}")
+#     # Calculate the threshold time for creating a thread
+#     threshold_time = now + timedelta(minutes=config.event_completion_threshold)
+#
+#     for event in guild.scheduled_events:
+#         # Check if the event has an end time and if it falls within the threshold
+#         ending_soon = event.end_time and now <= event.end_time <= threshold_time
+#
+#         if not ending_soon:
+#             print(f"Event '{event.name}' is not ending soon. End time: {event.end_time}")
+#             continue
+#
+#         print(f"Event '{event.name}' is ending soon. Creating a discussion thread...")
+#         channel = config.events_channel
+#         month_year = event.end_time.strftime("%m/%d")  # Ex. "12/31" for December 31st
+#         thread_name = f"{event.name} - ({month_year})"  # Ex. "Snek Den - (12/31)"
+#         thread = await channel.create_thread(
+#             name=thread_name,
+#             type=discord.ChannelType.public_thread,
+#             auto_archive_duration=config.auto_archive_duration
+#         )
+#         await thread.send(f"`{event.name}` is ending soon! Discuss here!")
+#         print(f"Created discussion thread '{thread_name}' for event '{event.name}' with ID {event.id}.")
+#
+#     print(f"Finished checking for scheduled events. Will check again in: {config.event_check_interval} minutes\n")
+#
+#
+# check_events.before_loop(bot.wait_until_ready)
 bot.run(config.token)
