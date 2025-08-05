9c0574c17d1ee64f630a004f77e7d93153266067
aman <amansomething@gmail.com>
2025-08-04 21:07:39 -0400

    Removed even more unneeded code

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from datetime import datetime, timezone, timedelta
-
 import discord
 from discord.ext import commands
 
 from config import config
-from event_messages import base_messages
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
@@ -33,15 +30,13 @@ async def on_ready():
 async def on_audit_log_entry_create(
         entry: discord.AuditLogEntry,
         channel: discord.TextChannel = None,
-        messages: dict = base_messages
 ):
     """
-    Check if a relevant action (scheduled event created, updated, or deleted) has occurred.
+    Check if a relevant action (scheduled event created or deleted) has occurred.
     Send a message to the team if so.
 
     :param entry: The entry that was added to the audit log.
     :param channel: The Discord channel to send messages to. Defaults to the channel from the config.
-    :param messages: The dictionary of base messages for different event types. See `event_messages.py`.
     """
     channel = channel or config.events_channel  # Use the channel from the config if not provided
     detected_action = entry.action
@@ -57,7 +52,6 @@ async def on_audit_log_entry_create(
     print(f"Relevant action detected: {detected_action}")
 
     action = relevant_actions[detected_action]
-    message = messages[action]
     url = entry.target.url if hasattr(entry.target, 'url') else None
 
     if hasattr(entry.target, 'name'):
@@ -70,16 +64,13 @@ async def on_audit_log_entry_create(
             print("Failed to retrieve event name from entry, using fallback.")
 
     if action == "Cancelled":
-        message.description = f"Event Name: `{name}`\n⎧ᴿᴵᴾ⎫ ❀◟(ᴗ_ ᴗ )"
-
-    embed = discord.Embed(title=message.title, description=message.description)
-    sent_message = await channel.send(embed=embed)  # Send embedded message for better formatting
-
-    if message.send_event_link:  # Also send a link to the event to generate a preview
+        title = f"Event Cancelled"
+        description = f"Event Name: `{name}`\n⎧ᴿᴵᴾ⎫ ❀◟(ᴗ_ ᴗ )"
+        embed = discord.Embed(title=title, description=description)
+        await channel.send(embed=embed)  # Send embedded message for better formatting
+    else:
         print(f"Sending event link: {url}")
         sent_message = await channel.send(f"[{name} Details]({url})")
-
-    if message.create_discussion_thread:
         print(f"Creating discussion thread for message: {sent_message.id}")
         await sent_message.create_thread(
             name=name,
